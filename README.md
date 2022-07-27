36735eb0669e4f491775d9a5ea47d0c36d3e0921
unknown <rinosaputra133@gmail.com>
2022-07-27 09:16:18 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-next to do: add feature
+# WAYSBUCK APP
+
+### Stacks
+- React.Js
+- React Context
+- React Bootstrap
+- Express Js
+- Mysql
+- Sequelize Js
+
+
+### Dependencies
+- React Router Dom
+- Axios
+- Multer
+- Json Webtoken
+- Bcrypt
+- Concurrently
+- Cors
+- Dotenv
+- Joi
+- Multer
+- Nodemon
+- Cloudinary
---
+++ server/seeders/20210724133318-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          email: 'admin@mail.com',
+          password:
+            '$2b$10$7ovHDrtaMe.FmutXxEhnWOo7rDOdTloUMgqms5RXYmL5/4dfM.OTm', //123456
+          name: 'admin',
+          rule: 'admin',
+          image: '../uploads/1645670953660-kattie.jpg'
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
